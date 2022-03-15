040202c2bfdf67ebc35fec762bd636a027f59c03
mhfwx <mahaofei001@gmail.com>
2022-03-15 19:02:28 -0400

    add new operate function

--- calcu.js
+++ calcu.js
@@ -13,3 +13,28 @@ const multiply = function(a,b){
 const divide = function(a,b){
     return a/b;
 }
+
+const operate = function(string){
+    const mapping={
+        '+':add,
+        '-':subtract,
+        '*':multiply,
+        '/':divide
+    };
+
+    const operators = ['+','-','*','/'];
+
+    for (oper of operators){
+        let input = string.split(oper);
+        if (input == string){
+            continue;
+        }else{
+            
+            let result = mapping[oper](Number(input[0]),Number(input[1]));
+            return Math.round(result*100)/100;
+        }
+    }
+    
+
+
+}
